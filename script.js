6fa55197867c0b0cc52bd1ffc98b1ec34f6714e6
zadohu <zadohu22@gmail.com>
2022-05-16 18:48:42 -0500

    makes hover work.

--- script.js
+++ script.js
@@ -13,22 +13,43 @@ function createGrid(input){
     container.style.gridTemplateColumns = gridTemplate;
     container.style.gridTemplateRows = gridTemplate;
     
+
     let iValue = input ** 2;
     for (i=0; i< iValue; i++){
+
         function createDivs(){
             let newDiv = document.createElement('div');
             newDiv.className = "gridItem";
             container.appendChild(newDiv);
             newDiv.innerText = "";
+            
+            
+            function hoverEffect(){
+                let x = false;
+                newDiv.addEventListener('mousedown', function(){
+                    newDiv.className = "gridItemHover";
+                    x = true;
+                })
+                
+                newDiv.addEventListener('mousemove', function(){
+                    if (x = true){
+                        newDiv.className = "gridItemHover";
+                    }
+                    
+                })
+            }
+            hoverEffect();
         }
         createDivs();
+
+    }      
+}   
         
-    }
+        
     
-}
+   
+        
+
+
+
 createGrid();
-
-
-
-
-
